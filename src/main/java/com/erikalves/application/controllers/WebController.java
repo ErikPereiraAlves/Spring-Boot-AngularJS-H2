4355738e011fa6c066df81632de148a4aabc68e1
erikpereiraalves <erikpalves@hotmail.com>
2018-02-14 14:12:22 +0100

    first version

--- src/main/java/com/erikalves/application/controllers/WebController.java
+++ src/main/java/com/erikalves/application/controllers/WebController.java
@@ -1,6 +1,8 @@
 package com.erikalves.application.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,8 +11,8 @@ public class WebController {
     @Controller
     public class MainController {
 
-        @RequestMapping(value = "/", method = RequestMethod.GET)
-        public String homepage() {
+        @RequestMapping(value="/",method = RequestMethod.GET)
+        public String homepage(){
             return "index";
         }
     }
